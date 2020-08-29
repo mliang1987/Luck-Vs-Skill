6e459dedd74556ed6970e4f7e371ae463301f422
Michael Liang <sliang76@gatech.edu>
2020-08-29 15:28:08 -0400

    Added multiple experiments

--- luckvsskill.py
+++ luckvsskill.py
@@ -1,6 +1,36 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def run_experiment(n = 100000, 
+def run_multiple_experiments(mn = 1000):
+	'''
+	Runs multiple experiments and aggregates the data.
+	Shows histogram of the "Lucked-out-Rate", which is a function of the number of people
+	who wouldn't have been selected if skill was the only factor.
+	'''
+	pop, cut, score = run_experiment()
+	scores = [score]
+
+	for _ in range(mn-1):
+		a, b, c = run_experiment()
+		pop = np.vstack((pop, a))
+		cut = np.vstack((cut, b))
+		scores.append(c)
+	
+	print("\n------------------------------------------------------")
+	print("For {} experiments:".format(mn))
+	print("------------------------------------------------------")
+	print("  Whole Population:")
+	print("    Skill: {}".format(np.mean(pop[:,1])))
+	print("    Luck:  {}".format(np.mean(pop[:,2])))
+	print("  Selected Top Population:")
+	print("    Skill: {}".format(np.mean(cut[:,1])))
+	print("    Luck:  {}".format(np.mean(cut[:,2])))
+	print("------------------------------------------------------\n")
+	_, plt.hist(scores, bins = 'auto')
+	plt.title("Histogram of Lucked Out Rate")
+	plt.show()
+
+def run_experiment(n = 500000, 
 	mu = 0.5, 
 	sigma = 0.1,
 	weight_skill = 0.95,
@@ -29,6 +59,7 @@ def run_experiment(n = 100000,
 	Returns:
 		all_means (ndarray) - Means of the features for the whole population
 		cutoff_means (ndarray) - Means of the features for the cutoff group
+		lucked_out_rate (float) - Percentage of selected who wouldn't have been if only skill-based
 	'''
 
 	# Generate n people with normal distribution of skill
@@ -59,6 +90,11 @@ def run_experiment(n = 100000,
 	all_means = np.mean(all_data, axis = 0)
 	cutoff_means = np.mean(cutoff_data, axis = 0)
 
+	# Extract comparison with skill isolated
+	skill_data = np.sort(all_data[:,1])[-cutoff:]
+	intersect_data = np.intersect1d(skill_data, cutoff_data[:,1])
+	lucked_out_rate = 1.0 - intersect_data.shape[0]/cutoff_data.shape[0]
+
 	# Print report to console if verbose:
 	if report:
 		print("\n------------------------------------------------------")
@@ -69,11 +105,15 @@ def run_experiment(n = 100000,
 		print("------------------------------------------------------")
 		print("Means (all):    score {}; skill {}; luck {}".format(*np.around(all_means*100, 2)))
 		print("Means (cutoff): score {}; skill {}; luck {}".format(*np.around(cutoff_means*100, 2)))
+		print("------------------------------------------------------")
+		print("Participants selected:", cutoff_data.shape[0])
+		print("Participants overlooked:", cutoff_data.shape[0]-intersect_data.shape[0])
 		print("------------------------------------------------------\n")
 
 	# Returns results
-	return all_means, cutoff_means
+	return all_means, cutoff_means, lucked_out_rate
 
 
 if __name__ == "__main__":
-	run_experiment(verbose = True, report = True)
+	run_multiple_experiments()
+	
